ca6a949fc92d8687473b5f93a48163a5dd10abdc
Jason Mullins <jason.m.mullins@gmail.com>
2015-08-19 14:39:59 -0400

    added toBoolean converter

---
+++ spec/addons/converters/toBoolean_spec.js
@@ -0,0 +1,135 @@
+var Snowman = require('../../../.');
+var toBooleanConverter = require('../../../src/addons/converters/toBoolean')();
+
+describe('toBoolean', function() {
+
+  var snowman, execSpy;
+
+  beforeEach(function() {
+    execSpy = {
+      onResolve:function() {},
+      onReject:function() {}
+    };
+    snowman = new Snowman({
+      _params:{
+        b1: true,
+        b2: 'true',
+        b3: false,
+        b4: 'false',
+        b5: undefined,
+        b6: null
+      }
+    });
+    spyOn(execSpy, 'onResolve');
+    spyOn(execSpy, 'onReject');
+  });
+
+  it('converts boolean true correctly for 1 field', function() {
+    var toBoolean = toBooleanConverter(
+      'b1',
+      {root:'_params'}
+    );
+    snowman
+    .pipe(toBoolean)
+    .exec(execSpy.onResolve, execSpy.onReject);
+    expect(execSpy.onResolve).toHaveBeenCalled();
+    expect(execSpy.onReject).not.toHaveBeenCalled();
+    expect(snowman.getData()._params.b1).toBe(true);
+  });
+
+  it('converts string true correctly for 1 field', function() {
+    var toBoolean = toBooleanConverter(
+      'b2',
+      {root:'_params'}
+    );
+    snowman
+    .pipe(toBoolean)
+    .exec(execSpy.onResolve, execSpy.onReject);
+    expect(execSpy.onResolve).toHaveBeenCalled();
+    expect(execSpy.onReject).not.toHaveBeenCalled();
+    expect(snowman.getData()._params.b2).toBe(true);
+  });
+
+  it('converts boolean false correctly for 1 field', function() {
+    var toBoolean = toBooleanConverter(
+      'b3',
+      {root:'_params'}
+    );
+    snowman
+    .pipe(toBoolean)
+    .exec(execSpy.onResolve, execSpy.onReject);
+    expect(execSpy.onResolve).toHaveBeenCalled();
+    expect(execSpy.onReject).not.toHaveBeenCalled();
+    expect(snowman.getData()._params.b3).toBe(false);
+  });
+
+  it('converts string false correctly for 1 field', function() {
+    var toBoolean = toBooleanConverter(
+      'b4',
+      {root:'_params'}
+    );
+    snowman
+    .pipe(toBoolean)
+    .exec(execSpy.onResolve, execSpy.onReject);
+    expect(execSpy.onResolve).toHaveBeenCalled();
+    expect(execSpy.onReject).not.toHaveBeenCalled();
+    expect(snowman.getData()._params.b4).toBe(false);
+  });
+
+  it('converts correctly for multiple field', function() {
+    var toBoolean = toBooleanConverter(
+      ['b1', 'b2', 'b3', 'b4'],
+      {root:'_params'}
+    );
+    snowman
+    .pipe(toBoolean)
+    .exec(execSpy.onResolve, execSpy.onReject);
+    expect(execSpy.onResolve).toHaveBeenCalled();
+    expect(execSpy.onReject).not.toHaveBeenCalled();
+    expect(snowman.getData()._params.b1).toBe(true);
+    expect(snowman.getData()._params.b2).toBe(true);
+    expect(snowman.getData()._params.b3).toBe(false);
+    expect(snowman.getData()._params.b4).toBe(false);
+  });
+
+  it('converts boolean true correctly for 1 field with from and to', function() {
+    var toBoolean = toBooleanConverter(
+      {from:'_params.b1', to:'out.b1'}
+    );
+    snowman
+    .pipe(toBoolean)
+    .exec(execSpy.onResolve, execSpy.onReject);
+    expect(execSpy.onResolve).toHaveBeenCalled();
+    expect(execSpy.onReject).not.toHaveBeenCalled();
+    expect(snowman.getData().out.b1).toBe(true);
+  });
+
+  it('converts string true correctly for 1 field with from and to', function() {
+    var toBoolean = toBooleanConverter(
+      {from:'_params.b2', to:'out.b2'}
+    );
+    snowman
+    .pipe(toBoolean)
+    .exec(execSpy.onResolve, execSpy.onReject);
+    expect(execSpy.onResolve).toHaveBeenCalled();
+    expect(execSpy.onReject).not.toHaveBeenCalled();
+    expect(snowman.getData()._params.b2).toBe('true');
+    expect(snowman.getData().out.b2).toBe(true);
+  });
+
+  it('converts correctly for array with from and to', function() {
+    var toBoolean = toBooleanConverter(
+      [
+        {from:'_params.b1', to:'out.b1'},
+        {from:'_params.b2', to:'out.b2'}
+      ]
+    );
+    snowman
+    .pipe(toBoolean)
+    .exec(execSpy.onResolve, execSpy.onReject);
+    expect(execSpy.onResolve).toHaveBeenCalled();
+    expect(execSpy.onReject).not.toHaveBeenCalled();
+    expect(snowman.getData().out.b1).toBe(true);
+    expect(snowman.getData().out.b2).toBe(true);
+  });
+});
---
+++ src/addons/converters/toBoolean.js
@@ -0,0 +1,30 @@
+module.exports = function() {
+
+  var dot = require('dot-object');
+
+  return function(input, options) {
+    return function() {
+      var opts = options || {};
+      var root = opts.root;
+      var errorPath = opts.errorPath || '_errors';
+      var data = this.getData();
+      var pathsOrObjs = Array.isArray(input) ? input : [input];
+      var val, from, to, fullFrom, fullTo;
+      pathsOrObjs.forEach(function(pathOrObj) {
+        if (typeof(pathOrObj) === 'object') {
+          from = pathOrObj.from;
+          to = pathOrObj.to || from;
+        } else {
+          to = from = pathOrObj;
+        }
+        fullFrom = root ? root + '.' + from : from;
+        fullTo = root ? root + '.' + to : to;
+        val = dot.pick(fullFrom, data);
+        if (val !== undefined && val !== null) {
+          dot.str(fullTo, (val === true || val === 'true' ? true : false), data);
+        }
+      });
+      this.resolve();
+    };
+  };
+};
