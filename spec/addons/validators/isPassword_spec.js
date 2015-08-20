d458d600f8e3bfa9a9fc76e103bf6a4c309f0e94
Jason Mullins <jason.m.mullins@gmail.com>
2015-08-20 12:15:16 -0400

    added password validator

---
+++ spec/addons/validators/isPassword_spec.js
@@ -0,0 +1,96 @@
+var Snowman = require('../../../.');
+var isPasswordValidator = require('../../../src/addons/validators/isPassword');
+
+describe('isPassword', function() {
+
+  var snowman, execSpy;
+
+  beforeEach(function() {
+    execSpy = {
+      onResolve:function() {},
+      onReject:function() {}
+    };
+    snowman = new Snowman({
+      _params:{
+        pw1: null,
+        pw2: undefined,
+        pw3: 'abc',
+        pw4: 'abcd',
+        pw5: 'abcde',
+        pw6: 'AbCd',
+        pw7: 'AbOpRRYYaa39389%#%@%^fsdf@#$%sfjffj1079656',
+        pw8: 'as39gjij98AA$%',
+        pw9: 'boot',
+        pw10: 'BooDaddy'
+      }
+    });
+    spyOn(execSpy, 'onResolve');
+    spyOn(execSpy, 'onReject');
+  });
+
+  it('succeeds', function() {
+    var isPassword = isPasswordValidator(['pw6', 'pw7', 'pw8'], {root:'_params'});
+    snowman
+    .pipe(isPassword)
+    .exec(execSpy.onResolve, execSpy.onReject);
+    expect(execSpy.onResolve).toHaveBeenCalled();
+    expect(execSpy.onReject).not.toHaveBeenCalled();
+  });
+
+  it('is simple word', function() {
+    var isPassword = isPasswordValidator('pw6', {root:'_params'});
+    snowman.$(isPassword).exec(execSpy.onResolve, execSpy.onReject);
+    expect(execSpy.onResolve).toHaveBeenCalled();
+    expect(snowman.getData()._meta.password.pw6.valid).toBe(true);
+    expect(snowman.getData()._meta.password.pw6.strength).toBe('simple');
+  });
+
+  it('is medium word', function() {
+    var isPassword = isPasswordValidator('pw10', {root:'_params'});
+    snowman.$(isPassword).exec(execSpy.onResolve, execSpy.onReject);
+    expect(execSpy.onResolve).toHaveBeenCalled();
+    expect(snowman.getData()._meta.password.pw10.valid).toBe(true);
+    expect(snowman.getData()._meta.password.pw10.strength).toBe('medium');
+  });
+
+  it('is strong word', function() {
+    var isPassword = isPasswordValidator('pw8', {root:'_params'});
+    snowman.$(isPassword).exec(execSpy.onResolve, execSpy.onReject);
+    expect(execSpy.onResolve).toHaveBeenCalled();
+    expect(snowman.getData()._meta.password.pw8.valid).toBe(true);
+    expect(snowman.getData()._meta.password.pw8.strength).toBe('strong');
+  });
+
+  it('is invalid', function() {
+    var isPassword = isPasswordValidator('pw3', {root:'_params'});
+    snowman.$(isPassword).exec(execSpy.onResolve, execSpy.onReject);
+    expect(execSpy.onReject).toHaveBeenCalled();
+    expect(snowman.getData()._meta.password.pw3.valid).toBe(false);
+    expect(snowman.getData()._meta.password.pw3.strength).toBe('simple');
+    expect(snowman.getData()._errors.pw3).toBe('is too short');
+  });
+
+  it('ignores undefined', function() {
+    var isPassword = isPasswordValidator('pw2', {root:'_params'});
+    snowman.$(isPassword).exec(execSpy.onResolve, execSpy.onReject);
+    expect(execSpy.onResolve).toHaveBeenCalled();
+  });
+
+  it('is invalid', function() {
+    var isPassword = isPasswordValidator('pw4', {root:'_params'});
+    snowman.$(isPassword).exec(execSpy.onResolve, execSpy.onReject);
+    expect(execSpy.onReject).toHaveBeenCalled();
+    expect(snowman.getData()._meta.password.pw4.valid).toBe(false);
+    expect(snowman.getData()._meta.password.pw4.strength).toBe('simple');
+    expect(snowman.getData()._errors.pw4).toBe('is too simple');
+  });
+
+  it('is invalid by setting min manually', function() {
+    var isPassword = isPasswordValidator('pw5', {root:'_params', min: 6});
+    snowman.$(isPassword).exec(execSpy.onResolve, execSpy.onReject);
+    expect(execSpy.onReject).toHaveBeenCalled();
+    expect(snowman.getData()._meta.password.pw5.valid).toBe(false);
+    expect(snowman.getData()._meta.password.pw5.strength).toBe('simple');
+    expect(snowman.getData()._errors.pw5).toBe('is too short');
+  });
+});
---
+++ src/addons/validators/isPassword.js
@@ -0,0 +1,33 @@
+module.exports = function(pathOrPaths, options) {
+
+  var dot = require('dot-object');
+  var passwordStrength = require('password-strength');
+
+  return function() {
+    var opts = options || {};
+    var root = opts.root;
+    var errorPath = opts.errorPath || '_errors';
+    var resultPath = opts.resultPath || '_meta.password';
+    passwordStrength.min = opts.min || 4;
+    var data = this.getData();
+    var paths = Array.isArray(pathOrPaths) ? pathOrPaths : [pathOrPaths];
+    var val, fullPath, err, pwResult;
+    paths.forEach(function(path) {
+      fullPath = root ? root + '.' + path : path;
+      val = dot.pick(fullPath, data);
+      if (val !== undefined && val !== null) {
+        pwResult = passwordStrength(val);
+        dot.str(resultPath + '.' + path, pwResult, data);
+        if (!pwResult.valid) {
+          err = true;
+          dot.str(errorPath + '.' + path, 'is ' + pwResult.hint, data);
+        }
+      }
+    });
+    if (err) {
+      this.reject();
+    } else {
+      this.resolve();
+    }
+  };
+};
